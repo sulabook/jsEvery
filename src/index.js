3ef5da466ba8b935d62aad4c865f2f45a829bdc0
sulabook <sula.book@gmail.com>
2022-04-03 10:41:05 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ const { ApolloServer, gql } = require('apollo-server-express');
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
+let notes = [
+    { id: '1', content: 'This is a note', author: 'Adam Scott' },
+    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 // Construct a schema, using GraphQL's schema language
 const typeDefs = gql`
   type Query {
     hello: String
+    notes: [Note!]!
+  }
+
+  type Note {
+      id: ID!
+      content: String!
+      author: String!
   }
 `;
 
 // Provide resolver functions for our schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    hello: () => 'Hello world!',
+    notes: () => notes
   }
+
 };
 
 const app = express();
